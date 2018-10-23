c85e118b3fd71a66a27850f841499e38acd02917
web小菜鸡 <38740836+1131153523@users.noreply.github.com>
2018-10-23 09:48:56 +0800

    Update mysql.js

--- config/mysql.js
+++ config/mysql.js
@@ -4,9 +4,9 @@ var wrapper = require('co-mysql'),
 
 var pool = mysql.createPool({
     host : 'localhost',
-    port : 3308,
+    port : 3306,
     database : 'mysite',
     user: 'root',
-    password : 'root'
+    password : '!Fyh138199183828'
 }), p = wrapper(pool);
 module.exports = p
